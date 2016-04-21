be77042ffda549ded8b5986d91da01c66486fb64
Sam DeGennaro <samdegennaro@Sams-MacBook-Pro.local>
2016-04-21 17:11:13 -0400

    adds relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,9 @@
 class Customer < ActiveRecord::Base
   belongs_to :waitstaff
+  has_many :food_orders
+  has_many :food_items, through: :food_orders
+
+
 
   #  attr_reader :party_size, :table_number
   def makeCustomers(size)
--- app/models/waitstaff.rb
+++ app/models/waitstaff.rb
@@ -1,3 +1,6 @@
 class Waitstaff < ActiveRecord::Base
   has_many :customers
+  has_many :food_orders
+  has_many :food_items, through: :food_orders
+
 end
