78f20c82188f0c6484c4dfa2c167aecf36f49398
Sam DeGennaro <samdegennaro@Sams-MacBook-Pro.local>
2016-04-22 11:36:26 -0400

    deletes parties views

--- app/models/table.rb
+++ app/models/table.rb
@@ -2,7 +2,7 @@ class Table < ActiveRecord::Base
   belongs_to :waitstaff
   has_many :food_orders
   has_many :food_items, through: :food_orders
-
+  validates :table_number, uniqueness: true
 
   #  attr_reader :party_size, :table_number
 
