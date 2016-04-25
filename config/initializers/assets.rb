841990d6e9f9304ceb054b2e48ec14a6374dbd3a
Sam DeGennaro <samdegennaro@Sams-MacBook-Pro.local>
2016-04-24 23:37:48 -0400

    removes duplicate migration

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( application.js )
-Rails.application.config.assets.precompile += %w( .js )
-Rails.application.config.assets.precompile += %w( red_things.css )
 Rails.application.config.assets.precompile += %w( application.css )
--- db/migrate/20160421193916_add_name_columnto_waitstaffs.rb
+++
@@ -1,6 +0,0 @@
-class AddNameColumntoWaitstaffs < ActiveRecord::Migration
-  def change
-    add_column :waitstaffs, :name, :string
-
-  end
-end
